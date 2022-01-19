052e5e456e9756c096cdf3083d3ec4de2c1758d4
Emil <emilpradeep@gmail.com>
2022-01-19 12:57:58 +0530

    added backend project and ui project

---
+++ oauth-sample/src/main/java/com/techie/oauthsample/OauthSampleApplication.java
@@ -0,0 +1,13 @@
+package com.techie.oauthsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthSampleApplication.class, args);
+	}
+
+}
---
+++ oauth-sample/src/test/java/com/techie/oauthsample/OauthSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techie.oauthsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
